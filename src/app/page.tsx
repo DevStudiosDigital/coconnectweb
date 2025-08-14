9ff4f4f8c555abb35a4627d93d2f1b807d408b49
usama samoo <usamasamoo786@gmail.com>
2025-08-15 02:47:06 +0500

    error fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,14 +67,13 @@ export default function Home() {
                 />
               </h3>
                 <p>
-                  Before anything else, we immerse ourselves in understanding your project's unique identity. This isn't a one-size-fits-all approach; it's a deep dive into your core values, your existing community's dynamics, and your precise growth objectives.
+                  {"Before anything else, we immerse ourselves in understanding your project's unique identity. This isn't a one-size-fits-all approach; it's a deep dive into your core values, your existing community's dynamics, and your precise growth objectives."}
                 </p>
                 <p>
-                  We sit down for a collaborative session to pinpoint exactly what you aim to achieve through partnerships, outlining your ideal target audience and preferred collaboration types.
-                  This comprehensive conceptualization phase allows us to craft a bespoke partnership strategy, acting as the essential blueprint for identifying truly synergistic connections that resonate with your long-term vision.
+                  {"We sit down for a collaborative session to pinpoint exactly what you aim to achieve through partnerships, outlining your ideal target audience and preferred collaboration types. This comprehensive conceptualization phase allows us to craft a bespoke partnership strategy, acting as the essential blueprint for identifying truly synergistic connections that resonate with your long-term vision."}
                 </p>
                 <p>
-                  It ensures every following step is perfectly aligned with your specific needs.
+                  {"It ensures every following step is perfectly aligned with your specific needs."}
                 </p>
             </div>
             <div className="group overflow-hidden relative border-b-[1px] border-primary pb-10 transition-all transform hover:-translate-y-2 hover:cursor-pointer">
@@ -90,13 +89,13 @@ export default function Home() {
                 />
               </h3>
                 <p>
-                  With a clear, tailored strategy in place, we activate our extensive, carefully curated network to identify the perfect collaborative partners for your project.
+                  {"With a clear, tailored strategy in place, we activate our extensive, carefully curated network to identify the perfect collaborative partners for your project."}
                 </p>
                 <p>
-                  Our process goes beyond superficial metrics; we're looking for projects that not only align seamlessly with your strategic vision but also share complementary goals and offer a genuine, mutually beneficial "win-win" scenario.This matchmaking stage involves detailed analysis to ensure cultural fit and audience overlap.
+                  {"Our process goes beyond superficial metrics; we're looking for projects that not only align seamlessly with your strategic vision but also share complementary goals and offer a genuine, mutually beneficial win-win scenario.This matchmaking stage involves detailed analysis to ensure cultural fit and audience overlap"}
                 </p>
                 <p>
-                  Once identified, we orchestrate the initial introductions, setting the optimal stage for productive conversations and laying the groundwork for partnerships that promise significant shared value and amplified reach.
+                  {"Once identified, we orchestrate the initial introductions, setting the optimal stage for productive conversations and laying the groundwork for partnerships that promise significant shared value and amplified reach."}
                 </p>
             </div>
             <div className="group overflow-hidden relative border-b-[1px] border-primary pb-10 transition-all transform hover:-translate-y-2 hover:cursor-pointer">
@@ -112,13 +111,13 @@ export default function Home() {
                 />
               </h3>
                 <p>
-                  Our commitment to your success extends far beyond initial introductions. We provide continuous, hands-on support throughout the entire lifecycle of your partnerships, acting as your dedicated navigators.
+                  {"Our commitment to your success extends far beyond initial introductions. We provide continuous, hands-on support throughout the entire lifecycle of your partnerships, acting as your dedicated navigators."}
                 </p>
                 <p>
-                  Whether you're facing early-stage coordination challenges, require assistance in structuring joint marketing campaigns, or need strategic advice on co-developing features or content, our team is actively involved.We proactively monitor progress, mediate discussions if needed, and offer practical solutions to ensure every collaborative effort runs smoothly and delivers tangible, impactful results.
+                  {"Whether you're facing early-stage coordination challenges, require assistance in structuring joint marketing campaigns, or need strategic advice on co-developing features or content, our team is actively involved.We proactively monitor progress, mediate discussions if needed, and offer practical solutions to ensure every collaborative effort runs smoothly and delivers tangible, impactful results."}
                 </p>
                 <p>
-                  Our goal is to ensure your partnerships not only launch successfully but also thrive and achieve their full potential.
+                  {"Our goal is to ensure your partnerships not only launch successfully but also thrive and achieve their full potential."}
                 </p>
             </div>
             <div className="group overflow-hidden relative border-b-[1px] border-primary pb-10 transition-all transform hover:-translate-y-2 hover:cursor-pointer">
@@ -134,14 +133,13 @@ export default function Home() {
                 />
               </h3>
                 <p>
-                  Transparency and data-driven insights are paramount to our process. At the close of each month, you'll receive comprehensive, easy-to-understand reports detailing the performance and impact of all your active partnerships
+                  {"Transparency and data-driven insights are paramount to our process. At the close of each month, you'll receive comprehensive, easy-to-understand reports detailing the performance and impact of all your active partnerships"}
                 </p>
                 <p>
-                  These reports offer a clear overview of key metrics, collaborative achievements, and community engagement results.
-                  This invaluable feedback loop allows us to collectively assess what's working best, identify areas for optimization, and fine-tune ongoing strategies to maximize their effectiveness.
+                  {"These reports offer a clear overview of key metrics, collaborative achievements, and community engagement results. This invaluable feedback loop allows us to collectively assess what's working best, identify areas for optimization, and fine-tune ongoing strategies to maximize their effectiveness."}
                 </p>
                 <p>
-                  It's a crucial step for ensuring continuous improvement, providing you with the clarity needed to make informed decisions and sustain your community's organic growth trajectory.
+                  {"It's a crucial step for ensuring continuous improvement, providing you with the clarity needed to make informed decisions and sustain your community's organic growth trajectory."}
                 </p>
             </div>
           </div>
